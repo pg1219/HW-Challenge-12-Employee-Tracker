a188f2c22590c23eb72274215e01d5100a02c0a8
pg1219 <peter.gronseth@gmail.com>
2023-05-23 23:43:52 -0400

    late tuesday push

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+);
